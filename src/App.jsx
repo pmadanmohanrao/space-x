d3a863d724e2a920b6a28d172ccf85aaf84d87b3
Madan Mohan <pmadanmohanrao@gmail.com>
2020-07-10 16:49:07 +0800

    changed App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">welcome to space-x</div>;
+  return <div className="App">welcome to space-x world</div>;
 }
 
 export default App;
